84be8e922c5e1fc4c7f505aa7212545688713dd8
Marcin <marcin.beldzinski@gmail.com>
2018-10-12 15:43:42 +0200

    program do obliczania roku przestępnego

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+#include "stdio.h"
+int main (){
+
+    int rok = 2007;
+    bool przestepny;
+    przestepny = !(rok % 4) && rok % 100 || !(rok % 400);
+
+    printf("%i", przestepny);
+
+
+}
